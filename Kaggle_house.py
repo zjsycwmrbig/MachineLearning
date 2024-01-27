2b299d388ecab9b4603df902b50506cd2f9f1af4
“zjsycwmrbig” <2057544748@qq.com>
2024-01-27 10:24:03 +0800

    多层感知机 Kaggle 基本实现 包含K折验证方法

--- Kaggle_house.py
+++ Kaggle_house.py
@@ -1,14 +1,103 @@
+import torch
 import pandas
 import Kaggle_download
 from Kaggle_download import DATA_HUB
+import torch.nn as nn
+import numpy as np
 
-DATA_HUB['kaggle_house_train'] = (Kaggle_download.DATA_URL + 'kaggle_house_pred_train.csv','585e9cc93e70b39160e7921475f9bcd7d31219ce')
-DATA_HUB['kaggle_house_test'] = (Kaggle_download.DATA_URL + 'kaggle_house_pred_test.csv','fa19780a7b011d9b009e8bff8e99922a8ee2eb90')
+DATA_HUB['kaggle_house_train'] = (
+    Kaggle_download.DATA_URL + 'kaggle_house_pred_train.csv', '585e9cc93e70b39160e7921475f9bcd7d31219ce')
+DATA_HUB['kaggle_house_test'] = (
+    Kaggle_download.DATA_URL + 'kaggle_house_pred_test.csv', 'fa19780a7b011d9b009e8bff8e99922a8ee2eb90')
 
 train_data = pandas.read_csv(Kaggle_download.download('kaggle_house_train'))
 test_data = pandas.read_csv(Kaggle_download.download('kaggle_house_test'))
 
-print(train_data.shape)
-print(test_data.shape)
+all_features = pandas.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+# 标准化数据
+all_features[numeric_features] = all_features[numeric_features].apply(lambda x: (x - x.mean()) / x.std())
+# 处理缺失值
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
+# 处理离散值
+all_features = pandas.get_dummies(all_features, dummy_na=True)
+# 转换为numpy数组
+all_features = all_features.astype(np.float32)
+# 转换为张量
+n_train = train_data.shape[0]
+train_features = torch.tensor(all_features[:n_train].values, dtype=torch.float32)
+test_features = torch.tensor(all_features[n_train:].values, dtype=torch.float32)
+train_labels = torch.tensor(train_data.SalePrice.values.reshape(-1, 1), dtype=torch.float32)
 
-print(train_data.iloc[0:4, [0, 1, 2, 3, -3, -2, -1]])
+loss = nn.MSELoss()
+in_features = train_features.shape[1]
+
+
+def get_net():
+    net = nn.Sequential(nn.Linear(in_features, 1))
+    return net
+
+
+def log_rmse(net, features, labels):
+    # 将小于1的值设置为1，使得取对数时数值更稳定 clamp 是限制的意思
+    clipped_preds = torch.clamp(net(features), 1, float('inf'))
+    rmse = torch.sqrt(loss(clipped_preds.log(), labels.log()))
+    return rmse.item()
+
+
+def train(net, train_features, train_labels, test_features, test_labels, num_epochs, learning_rate, weight_decay,
+          batch_size):
+    train_ls, test_ls = [], []
+    dataset = torch.utils.data.TensorDataset(train_features, train_labels)
+    train_iter = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True)
+    optimizer = torch.optim.Adam(params=net.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            optimizer.zero_grad()
+            l = loss(net(X), y)
+            l.backward()
+            optimizer.step()
+        train_ls.append(log_rmse(net, train_features, train_labels))
+        if test_labels is not None:
+            test_ls.append(log_rmse(net, test_features, test_labels))
+    return train_ls, test_ls
+
+
+def get_k_fold_data(k, i, X, y):
+    # 返回第i折交叉验证时所需要的训练和验证数据
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        # slice 是切片的意思
+        idx = slice(j * fold_size, (j + 1) * fold_size)
+        X_part, y_part = X[idx, :], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            # cat 是concatenate的意思
+            X_train = torch.cat((X_train, X_part), dim=0)
+            y_train = torch.cat((y_train, y_part), dim=0)
+    return X_train, y_train, X_valid, y_valid
+
+
+def k_fold(k, X_train, y_train, num_epochs, learning_rate, weight_decay, batch_size):
+    train_l_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        net = get_net()
+        train_ls, valid_ls = train(net, *data, num_epochs, learning_rate, weight_decay, batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        if i == 0:
+            pass
+        print(f'fold {i + 1}, train rmse {train_ls[-1]:.6f}, valid rmse {valid_ls[-1]:.6f}')
+    return train_l_sum / k, valid_l_sum / k
+
+
+k, num_epochs, lr, weight_decay, batch_size = 5, 100, 5, 0, 64
+train_l, valid_l = k_fold(k, train_features, train_labels, num_epochs, lr, weight_decay, batch_size)
+print(f'{k}-fold validation: avg train rmse {train_l:.6f}, avg valid rmse {valid_l:.6f}')
